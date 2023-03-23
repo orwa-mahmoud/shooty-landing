43db3adc8dda06799e200a51b3453f70bbf8bb23
musta-sd <118274352+musta-sd@users.noreply.github.com>
2023-03-23 09:52:52 +0400

    feat : fetch feature content from an API

--- components/Features.js
+++ components/Features.js
@@ -1,8 +1,37 @@
-import React, { useState } from 'react'
+import React,{ useEffect,useState } from 'react'
 import Image from 'next/image'
+import axios from "axios";
 function Features() {
 
     const [activeId, setActiveId] = useState('tab1');
+    const [tabs, setTabs]         = useState([]);
+
+    const [featureSectionContent, setFeatureSectionContent]         = useState([]);
+    const [mainFeatureSectionContent, setMainFeatureSectionContent] = useState([]);
+
+    const getFeatureSectionContent = async (data) => {
+
+        await axios.get("/api/featureSectionContent", data,
+            {
+                headers: {'Content-Type': 'application/json'},
+                withCredentials: true
+            }
+        )
+        .then(async function (response) {
+            setTabs(response.data.tabs)
+            setFeatureSectionContent(response.data.featureSectionContent)
+            setMainFeatureSectionContent(response.data.mainFeatureSectionContent)
+        })
+        .catch(function (error) {
+            console.log('items error:',error);
+        })
+    }
+
+    useEffect(() => {
+        getFeatureSectionContent()
+    },[]);
+
+
 
     const values = [
         { id: "tab1", text: "Capitalize On Custom" },
@@ -21,14 +50,14 @@ function Features() {
             <div className="container">
                 <div className="row text-center">
                     <div className="col-12">
-                        <h1 data-aos="fade-up">Integrations and API endpoints for compatibility. And opportunity.</h1>
-                        <p data-aos="fade-up">Workflow software for content creation at scale</p>
+                        <h1 data-aos="fade-up">{mainFeatureSectionContent.headLine}</h1>
+                        <p data-aos="fade-up">{mainFeatureSectionContent.body}</p>
                     </div>
                 </div>
                 <div className="row feture-tabs">
                     <div className="col-12">
                         <ul className="nav nav-pills mb-3">
-                        {values.map((val,index) => (
+                        {tabs.map((val,index) => (
                             <li className={"nav-link" + (activeId === val.id ? " active" : "")}
                                 data-bs-toggle="pill" href="#tab1"
                                 onClick={(e) => changeTab(e,val.id)} key={index}>
@@ -37,74 +66,26 @@ function Features() {
                             ))}
                         </ul>
                         <div className="tab-content">
-                            <div className={"tab-pane fade show"+ (activeId === 'tab1' ? " active" : "")} id="tab1">
-                                <div className="row d-flex flex-wrap align-items-center">
-                                    <div className="col-lg-6">
-                                        <h3 className="mt-4" data-aos="fade-up">Capitalize On Custom</h3>
-                                        <p className="mt-4" data-aos="fade-up">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commo
-                                        </p>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <Image src="/assets/img/laptop.png" fill className="img-fluid custom-img" alt="Slider"
-                                            data-aos="zoom-out" data-aos-delay="200"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={"tab-pane fade show"+ (activeId === 'tab2' ? " active" : "")} id="tab2">
-                                <div className="row d-flex flex-wrap align-items-center">
-                                    <div className="col-lg-6">
-                                        <h3 className="mt-4" data-aos="fade-up">eCommerce Trends</h3>
-                                        <p className="mt-4" data-aos="fade-up">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commo
-                                        </p>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <Image src="/assets/img/laptop.png" fill className="img-fluid custom-img" alt="Slider"
-                                            data-aos="zoom-out" data-aos-delay="200"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={"tab-pane fade show"+ (activeId === 'tab3' ? " active" : "")} id="tab3">
-                                <div className="row d-flex flex-wrap align-items-center">
-                                    <div className="col-lg-6">
-                                        <h3 className="mt-4" data-aos="fade-up">On Demand Platforms</h3>
-                                        <p className="mt-4" data-aos="fade-up">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commo
-                                        </p>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <Image src="/assets/img/laptop.png" fill className="img-fluid custom-img" alt="Slider"
-                                            data-aos="zoom-out" data-aos-delay="200"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={"tab-pane fade show"+ (activeId === 'tab4' ? " active" : "")} id="tab4">
-                                <div className="row d-flex flex-wrap align-items-center">
-                                    <div className="col-lg-6">
-                                        <h3 className="mt-4" data-aos="fade-up">Growth Trends</h3>
-                                        <p className="mt-4" data-aos="fade-up">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commo
-                                        </p>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <Image src="/assets/img/laptop.png" fill className="img-fluid custom-img" alt="Slider"
-                                            data-aos="zoom-out" data-aos-delay="200"/>
-                                    </div>
-                                </div>
-                            </div>
+                            {
+                                featureSectionContent.map((item,index) => {
+                                    return (
+                                        <div key={index} className={"tab-pane fade show"+ (activeId === item.id ? " active" : "")} id={item.id}>
+                                            <div className="row d-flex flex-wrap align-items-center">
+                                                <div className="col-lg-6">
+                                                    <h3 className="mt-4" data-aos="fade-up">{item.headLine}</h3>
+                                                    <p className="mt-4" data-aos="fade-up">{item.body}</p>
+                                                </div>
+                                                <div className="col-lg-6">
+                                                {item.image && (
+                                                    <Image src={item.image} fill className="img-fluid custom-img" alt="Slider"
+                                                    data-aos="zoom-out" data-aos-delay="200"/>
+                                                )}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                 </div>
